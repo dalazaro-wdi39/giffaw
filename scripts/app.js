b025ed5ba72352f85c726f71e523f3282c33e9e4
dalazaro <daryl.lazaro@gmail.com>
2017-07-06 22:03:03 -0700

    created working page of top 25 gifs

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,44 @@
 $(document).on("ready", function(){
 
+  $('form').on('submit', function(event){
+    event.preventDefault();
+
+    $.ajax({
+
+    	// What kind of request
+    	method: "GET",
+
+    	// The URL for the request
+    	url: "http://api.giphy.com/v1/gifs/search",
+
+      dataType: 'json',
+
+    	// The data to send aka query parameters
+    	data: $('form').serialize(),
+
+    	// Code to run if the request succeeds;
+    	// the response is passed to the function
+    	success: onSuccess,
+
+    	// Code to run if the request fails; the raw request and
+    	// status codes are passed to the function
+    	error: onError
+    });
+
+    function onSuccess(response){
+      for (i = 0; i <= 25; i++){
+    	   $("div.gif-gallery").append(`<img src="${response.data[i].images.fixed_height.url}">`);
+      };
+    }
+
+
+    function onError(xhr, status, errorThrown) {
+    	alert("Sorry, there was a problem!");
+    	console.log("Error: " + errorThrown);
+    	console.log("Status: " + status);
+    	console.dir(xhr);
+    }
+
+  });
+
 });
